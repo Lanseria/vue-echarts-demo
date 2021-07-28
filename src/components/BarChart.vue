35699a332d26f8882965cd50c0dedda684ab782a
Lanseria <zhangchao564265135@hotmail.com>
2021-07-28 11:24:46 +0800

    添加正态bar图

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -2,7 +2,8 @@
   <h2 id="bar" class="text-center text-lg text-gray-600 p-5">
     <a href="#bar">Bar chart (with async data & custom theme)</a>
   </h2>
-  <v-chart class="chart" :option="option" />
+  <v-chart class="chart" :option="option1" />
+  <v-chart class="chart" :option="option2" @zr:click="handleZrClick" />
 </template>
 
 <script lang="ts">
@@ -17,7 +18,8 @@ import {
   DatasetComponent,
 } from "echarts/components";
 import VChart from "vue-echarts";
-import { ref, defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import getBarData from "../data/bar";
 
 use([
   CanvasRenderer,
@@ -36,33 +38,49 @@ export default defineComponent({
   },
   provide: {},
   setup() {
-    const option = ref({
-      legend: {},
-      tooltip: {},
-      dataset: {
-        // 用 dimensions 指定了维度的顺序。直角坐标系中，
-        // 默认把第一个维度映射到 X 轴上，第二个维度映射到 Y 轴上。
-        // 如果不指定 dimensions，也可以通过指定 series.encode
-        // 完成映射，参见后文。
-        dimensions: ["product", "2015", "2016", "2017"],
-        source: [
-          { product: "Matcha Latte", "2015": 43.3, "2016": 85.8, "2017": 93.7 },
-          { product: "Milk Tea", "2015": 83.1, "2016": 73.4, "2017": 55.1 },
-          { product: "Cheese Cocoa", "2015": 86.4, "2016": 65.2, "2017": 82.5 },
-          {
-            product: "Walnut Brownie",
-            "2015": 72.4,
-            "2016": 53.9,
-            "2017": 39.1,
-          },
-        ],
+    const option2 = ref(getBarData());
+    const handleZrClick = () => {
+      option2.value = getBarData();
+    };
+    return {
+      handleZrClick,
+      option1: {
+        legend: {},
+        tooltip: {},
+        dataset: {
+          // 用 dimensions 指定了维度的顺序。直角坐标系中，
+          // 默认把第一个维度映射到 X 轴上，第二个维度映射到 Y 轴上。
+          // 如果不指定 dimensions，也可以通过指定 series.encode
+          // 完成映射，参见后文。
+          dimensions: ["product", "2015", "2016", "2017"],
+          source: [
+            {
+              product: "Matcha Latte",
+              "2015": 43.3,
+              "2016": 85.8,
+              "2017": 93.7,
+            },
+            { product: "Milk Tea", "2015": 83.1, "2016": 73.4, "2017": 55.1 },
+            {
+              product: "Cheese Cocoa",
+              "2015": 86.4,
+              "2016": 65.2,
+              "2017": 82.5,
+            },
+            {
+              product: "Walnut Brownie",
+              "2015": 72.4,
+              "2016": 53.9,
+              "2017": 39.1,
+            },
+          ],
+        },
+        xAxis: { type: "category" },
+        yAxis: {},
+        series: [{ type: "bar" }, { type: "bar" }, { type: "bar" }],
       },
-      xAxis: { type: "category" },
-      yAxis: {},
-      series: [{ type: "bar" }, { type: "bar" }, { type: "bar" }],
-    });
-
-    return { option };
+      option2,
+    };
   },
 });
 </script>
--- src/data/bar.ts
+++ src/data/bar.ts
@@ -1,45 +1,52 @@
-function random() {
-  return Math.round(300 + Math.random() * 700) / 10;
+import { groupBy, mapValues, toPairs, chain } from "lodash";
+function getNumberInNormalDistribution(mean: number, stdDev: number) {
+  return mean + randomNormalDistribution() * stdDev;
+}
+
+function randomNormalDistribution() {
+  let u = 0.0,
+    v = 0.0,
+    w = 0.0,
+    c = 0.0;
+  do {
+    //获得两个（-1,1）的独立随机变量
+    u = Math.random() * 2 - 1.0;
+    v = Math.random() * 2 - 1.0;
+    w = u * u + v * v;
+  } while (w == 0.0 || w >= 1.0);
+  //这里就是 Box-Muller转换
+  c = Math.sqrt((-2 * Math.log(w)) / w);
+  //返回2个标准正态分布的随机数，封装进一个数组返回
+  //当然，因为这个函数运行较快，也可以扔掉一个
+  //return [u*c,v*c];
+  return u * c;
 }
 
 export default function getBarData() {
+  const data = Array.apply(null, Array(100000)).map(() => {
+    return getNumberInNormalDistribution(0, 100);
+  });
+  const dataSource = chain(data)
+    .groupBy(Math.floor)
+    .mapValues("length")
+    .toPairs()
+    .value()
+    .map((m) => {
+      return {
+        height: m[0],
+        value: m[1],
+      };
+    });
   return {
-    textStyle: {
-      fontFamily: 'Inter, "Helvetica Neue", Arial, sans-serif',
-    },
+    tooltip: {},
     dataset: {
-      dimensions: ["Product", "2015", "2016", "2017"],
-      source: [
-        {
-          Product: "Matcha Latte",
-          2015: random(),
-          2016: random(),
-          2017: random(),
-        },
-        {
-          Product: "Milk Tea",
-          2015: random(),
-          2016: random(),
-          2017: random(),
-        },
-        {
-          Product: "Cheese Cocoa",
-          2015: random(),
-          2016: random(),
-          2017: random(),
-        },
-        {
-          Product: "Walnut Brownie",
-          2015: random(),
-          2016: random(),
-          2017: random(),
-        },
-      ],
+      dimensions: ["height", "value"],
+      source: dataSource,
     },
-    xAxis: { type: "category" },
-    yAxis: {},
+    xAxis: { type: "value" },
+    yAxis: { axisLine: { show: false }, axisTick: { show: false } },
     // Declare several bar series, each will be mapped
     // to a column of dataset.source by default.
-    series: [{ type: "bar" }, { type: "bar" }, { type: "bar" }],
+    series: [{ type: "bar" }],
   };
 }
